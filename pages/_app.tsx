953ccba729aac4e5cd7b9cf6e39ce39eb8a517ce
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-11 14:22:19 +0200

    added prettier and basic navbar and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,43 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
-    <div>
-     <h1>Siemanko poniedziałkowy poranku</h1>
-    </div>
-  )
-}
+    <section className="bg-gray-50">
+      <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+        <div className="mx-auto max-w-3xl text-center">
+          <h1 className="text-3xl font-extrabold sm:text-5xl">
+            Understand User Flow.
+            <strong className="font-extrabold text-red-700 sm:block">
+              Increase Conversion.
+            </strong>
+          </h1>
 
-export default Home
+          <p className="mt-4 sm:text-xl sm:leading-relaxed">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
+            illo tenetur fuga ducimus numquam ea!
+          </p>
+
+          <div className="mt-8 flex flex-wrap justify-center gap-4">
+            <a
+              className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+              href="/get-started"
+            >
+              Get Started
+            </a>
+
+            <a
+              className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+              href="/about"
+            >
+              Learn More
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
