692b35b2f52fa59c10aa00188d7012114a675614
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-12 22:02:01 +0200

    added fetcher helper function

---
+++ utils/fetcher.ts
@@ -0,0 +1,7 @@
+const BASE_API_URL = 'https://naszsklep-api.vercel.app/api';
+
+export const fetcher = async <T>(path: string) => {
+  const response = await fetch(`${BASE_API_URL}${path}`);
+  const data: T = await response.json();
+  return data;
+};
