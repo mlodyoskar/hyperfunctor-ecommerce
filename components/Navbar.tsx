858c2070c02178939730912747abb604429ac5be
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-12 22:33:50 +0200

    product page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const navigationLinks = [
   },
   {
     text: 'products',
-    to: '/products',
+    to: '/',
   },
   {
     text: 'contact',
@@ -29,9 +29,9 @@ export const Navbar = () => {
               stroke="currentColor"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h16M4 18h16"
               />
             </svg>
@@ -73,9 +73,9 @@ export const Navbar = () => {
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                     />
                   </svg>
@@ -97,9 +97,9 @@ export const Navbar = () => {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                     />
                   </svg>
@@ -121,9 +121,9 @@ export const Navbar = () => {
                     stroke="currentColor"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                     />
                   </svg>
--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,14 @@ const Home = () => {
   }
 
   return (
-    <section className="bg-gray-50">
-      <h1>All products</h1>
+    <section className="bg-gray-50 flex flex-col max-w-7xl mx-auto">
+      <div className="mb-4">
+        <span className="inline-block h-1 w-12 bg-red-700"></span>
+
+        <h2 className="mt-1 text-2xl font-extrabold uppercase tracking-wide lg:text-3xl">
+          All products
+        </h2>
+      </div>
       <ProductList products={data} />
     </section>
   );
