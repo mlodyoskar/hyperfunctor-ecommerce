336075af38914c0f3aaf5030b01ee4c1949c3474
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-10-12 21:16:01 +0200

    added react-query to project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Navbar } from '../components/Navbar';
 import { Footer } from '../components/Footer';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
-      <main className="min-h-screen">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <main className="min-h-screen">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </QueryClientProvider>
     </>
   );
 }
