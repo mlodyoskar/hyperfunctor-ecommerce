75a3e5eb6fd015b887f85cd3bddefa120366031e
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-11-15 13:11:23 +0100

    feat: config in codegen to generates hooks

--- codegen.ts
+++ codegen.ts
@@ -1,18 +1,35 @@
-import type { CodegenConfig } from '@graphql-codegen/cli';
+// import type { CodegenConfig } from '@graphql-codegen/cli';
+
+// const config: CodegenConfig = {
+// 	overwrite: true,
+// 	schema: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl9xeq43u0onx01tbf4j774ch/master',
+// 	documents: 'graphql/*.graphql',
+// 	generates: {
+// 		'generated/': {
+// 			preset: 'client',
+// 			plugins: ['typescript-react-apollo', 'typescript', 'typescript-operations'],
+// 		},
+// 		'./graphql.schema.json': {
+// 			plugins: ['introspection'],
+// 		},
+// 	},
+// };
+
+// export default config;
+
+import { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-	overwrite: true,
 	schema: 'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cl9xeq43u0onx01tbf4j774ch/master',
-	documents: 'graphql/*.graphql',
+	documents: ['graphql/*.graphql'],
+	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
-		'generated/': {
-			preset: 'client',
-			plugins: [],
-		},
-		'./graphql.schema.json': {
-			plugins: ['introspection'],
+		'./generated/graphql.tsx': {
+			// preset: 'client',
+			plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
 		},
 	},
+	overwrite: true,
 };
 
 export default config;
