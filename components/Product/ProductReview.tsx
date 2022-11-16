ee754525c06350f82183755b0c9ab62d712bc6fb
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-11-16 12:34:12 +0100

    fix: small fix to review form padding

--- components/Product/ProductReview.tsx
+++ components/Product/ProductReview.tsx
@@ -51,7 +51,7 @@ export const ProductReview = ({ productSlug }: Props) => {
 
 	return (
 		<section>
-			<div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+			<div className="mx-auto max-w-screen-xl py-16 sm:px-6 lg:px-8">
 				<div className="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
 					<div className="lg:col-span-2 lg:py-12">
 						<p className="max-w-xl text-lg">
