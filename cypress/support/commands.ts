e166522ab0d51b1acdbde80dd8a0730746cbaa1a
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-12-12 13:13:49 +0100

    fix: last fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,5 @@
-/* eslint-disable @typescript-eslint/no-namespace */
+export {};
+
 /// <reference types="cypress" />
 // ***********************************************
 // This example commands.ts shows you how to
