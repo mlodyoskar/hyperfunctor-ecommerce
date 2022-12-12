79291b073d96251e9faea5ebae11b7354fe11b6d
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-12-12 13:04:09 +0100

    test: adding product to cart flow

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
+		viewportWidth: 1920,
+		viewportHeight: 1080,
 		baseUrl: 'http://localhost:3000',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
