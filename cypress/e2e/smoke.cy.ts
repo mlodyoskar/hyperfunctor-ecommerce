d13865a462392ca32f02d2845f56b5e972d24d69
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-12-12 11:11:19 +0100

    feat: added cypress and basic tests

---
+++ cypress/e2e/smoke.cy.ts
@@ -0,0 +1,6 @@
+describe('Smoke tests', () => {
+	it('Checks if header is visible on main page', () => {
+		cy.visit('/');
+		cy.get('h1').should('have.text', 'Understand User Flow.Increase Conversion.');
+	});
+});
